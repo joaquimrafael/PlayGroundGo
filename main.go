17f87ab0b2e2f329b0c34e57b4044d79a732e196
joaquimprieto <joaquim.mpp@outlook.com>
2026-06-28 23:15:18 -0300

    Add initial implementation of rights declarations and number addition functionality

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//go:embed english_rights.txt
+var englishRights string
+
+//go:embed french_rights.txt
+var frenchRights string
+
+func process(args []string) {
+	if len(args) == 1 {
+		fmt.Println("no language provided")
+		return
+	}
+	switch strings.ToLower(args[1]) {
+	case "english":
+		fmt.Println(englishRights)
+	case "french":
+		fmt.Println(frenchRights)
+	default:
+		fmt.Println("unknown language:", os.Args[1])
+	}
+}
+
+func main() {
+	process(os.Args)
+}
